e22f035137e71ec76dba13640f9574c6b88d7e4b
Sergey <spapoyan1987@gmail.com>
2022-12-19 17:17:40 +0400

    commit

--- src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
@@ -1,6 +1,9 @@
 package school.mjc.stage0.loops.finalTask;
 
 public class MultiplicationTable {
-    public void printTable(int numberTableToPrint){
+    public void printTable(int numberTableToPrint) {
+        for (int j = 1; j <= 10; j++) {
+            System.out.println(j + " x " + numberTableToPrint + " = " + j * numberTableToPrint);
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,10 +2,24 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
+        if (cathetusLength <= 0) {
+            return;
+        }
+        for (int i = 1; i <= cathetusLength; i++) {
+            for (int j = 1; j <= cathetusLength - i; j++)
+                System.out.print(" ");
+            for (int k = i; k >= 1; k--)
+                System.out.print((k >= 10) ? +k : "" + k);
+            for (int k = 2; k <= i; k++)
+                System.out.print((k >= 10) ? +k : "" + k);
+            System.out.println();
+        }
 
     }
 
+
     public static void main(String[] args) {
         new Pyramid().printPyramid(7);
+
     }
 }
